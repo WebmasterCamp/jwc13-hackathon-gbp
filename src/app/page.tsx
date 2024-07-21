8d5f4cb0cf75e3c43bac6515b1c1f9b90192ae0a
SmartMedicalRoom <smart.medical.room@gmail.com>
2024-07-21 13:01:27 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import Image from "next/image";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <ThemeToggle />
+    <div className="flex flex-col">
+      <div className="flex flex-row">
+      <div className="flex flex-col">
+        <p>เนื้อหา</p>
+        <h1>hsdfzg.iu</h1>
+      </div>
+      <div>
+        <img src="cabibara.webp" alt="" />
+      </div>
+      </div>
+      <div className="flex flex-row">
+        <h1>รายละเอียดเว้ปไวต์</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo reprehenderit alias natus, enim atque corporis ratione esse expedita ea facilis! Dolorem eos animi sapiente necessitatibus cum! Unde animi vitae veniam!</p>
+      </div>
     </div>
   );
 }
